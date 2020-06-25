b331552d5579b4ea84789cb11b9bcd4748cb9bf3
mrvalterhugo <62514801+mrvalterhugo@users.noreply.github.com>
2020-06-25 11:20:53 +0100

    Update Valter_Schmaltz_python.py

--- Valter_Schmaltz_python.py
+++ Valter_Schmaltz_python.py
@@ -2,24 +2,6 @@ import shutil
 import csv
 import os
 from datetime import date
-'''
-Python Assessment - Amazon Career - Cloud Support Engineer Course
-Autor - Valter Hugo Schmaltz
-Email - valterschmaltz@hotmail.com
-Version 1.0
-This is a program to manipulate a data file,
-listing , adding/removing/updating and backing up data.
-The file data.txt needs to exist in the same directory as this program.
-Data is separeted by tabs.
-The program is divided by functions, which are called from the Main Menu:
-list_users()
-add_user()
-remove_user()
-user_info()
-update_info()
-backup()
-
-'''
 #### This function reads and prints the data inside the file #####
 def list_users():
     with open('data.txt') as f: file_r = f.readlines()
